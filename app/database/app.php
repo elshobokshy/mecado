0d71ca18f487e48f46ef611e2d9121a34203d9ca
LouisCuny <louis-cuny@orange.fr>
2017-11-06 16:09:37 +0100

    Adding the php script for our custom app tables (Using Manager::Schema)

---
+++ app/database/app.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Capsule\Manager;
+use Illuminate\Database\Schema\Blueprint;
+
+Manager::schema()->create('list', function (Blueprint $table) {
+    $table->increments('id');
+    $table->string('name');
+    $table->string('description');
+    $table->string('token');
+    $table->date('date');
+    $table->string('recipient');
+    $table->unsignedInteger('user_id');
+    $table->timestamps();
+    $table->foreign('user_id')->references('id')->on('user');
+});
+
+Manager::schema()->create('gift', function (Blueprint $table) {
+    $table->increments('id');
+    $table->unsignedInteger('list_id');
+    $table->string('name');
+    $table->string('url')->nullable();
+    $table->string('description')->nullable();
+    $table->integer('price')->nullable();
+    $table->string('picture')->nullable();
+    $table->boolean('booked');
+    $table->timestamps();
+    $table->foreign('list_id')->references('id')->on('list');
+});
+
+Manager::schema()->create('comment_list', function (Blueprint $table) {
+    $table->increments('id');
+    $table->string('author');
+    $table->string('content');
+    $table->unsignedInteger('list_id');
+    $table->timestamps();
+    $table->foreign('list_id')->references('id')->on('list');
+});
+
+Manager::schema()->create('comment_gift', function (Blueprint $table) {
+    $table->increments('id');
+    $table->string('author');
+    $table->string('content');
+    $table->unsignedInteger('gift_id');
+    $table->timestamps();
+    $table->foreign('gift_id')->references('id')->on('gift');
+});
