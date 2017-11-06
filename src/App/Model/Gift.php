803adbe276f8b620105f529a9c7876d327214c7a
LouisCuny <louis-cuny@orange.fr>
2017-11-06 16:42:06 +0100

    Eloquent relationchips TRY 1

--- src/App/Model/Gift.php
+++ src/App/Model/Gift.php
@@ -7,4 +7,13 @@ class Gift extends Model
 {
     protected $table = 'gift';
 
+    public function commentgift()
+    {
+        return $this->hasMany('Commentgift');
+    }
+
+    public function giftlist()
+    {
+        return $this->belongsTo('Giftlist');
+    }
 }
--- src/App/Model/Giftlist.php
+++ src/App/Model/Giftlist.php
@@ -7,4 +7,18 @@ class Giftlist extends Model
 {
     protected $table = 'giftlist';
 
+    public function commentlist()
+    {
+        return $this->hasMany('Commentlist');
+    }
+
+    public function gift()
+    {
+        return $this->hasMany('Gift');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('User');
+    }
 }
