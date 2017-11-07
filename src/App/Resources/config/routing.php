5b82e89e6d5d01c59a206287d8c2936faf8515fa
Islam Elshobokshy <islam20088@hotmail.com>
2017-11-07 14:04:50 +0100

    Ability to create a giftlist

--- src/App/Resources/config/routing.php
+++ src/App/Resources/config/routing.php
@@ -6,4 +6,5 @@ $app->get('/myaccount', 'app.controller:myaccount')->setName('myaccount');
 $app->post('/myaccount', 'app.controller:editProfile');
 $app->get('/mylists', 'list.controller:mylists')->setName('mylists');
 $app->get('/addlist', 'list.controller:addlist')->setName('addlist');
+$app->post('/addlist', 'list.controller:addlist');
 $app->get('/about','app.controller:about')->setName('About');
